9dc7c346b6077afee069bcc10265e8b01e8e4c1c
Fuzzwah <fuzzwah@github.com>
2019-06-16 09:10:07 +1000

    version bump

--- irsdk.py
+++ irsdk.py
@@ -15,7 +15,7 @@ try:
 except ImportError:
     from yaml import Loader as YamlLoader
 
-VERSION = '1.2.3'
+VERSION = '1.2.4'
 
 SIM_STATUS_URL = 'http://127.0.0.1:32034/get_sim_status?object=simStatus'
 
